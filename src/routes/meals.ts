95090395c46d943973b1de71b1aeb42e49bba248
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-11-29 09:07:33 -0300

    chore: create initial structure of project

---
+++ src/routes/meals.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    return await knex('sqlite_schema').select('*')
+  })
+}
