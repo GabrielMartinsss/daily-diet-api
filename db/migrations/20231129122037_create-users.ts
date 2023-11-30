ef390fe0480bd6d674769bcfd257b781248efe51
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-11-30 06:52:49 -0300

    fix: create table users and meals

--- db/migrations/20231129122037_create-users.ts
+++ db/migrations/20231129122037_create-users.ts
@@ -7,18 +7,8 @@ export async function up(knex: Knex): Promise<void> {
     table.string('email').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
-
-  await knex.schema.createTable('meals', (table) => {
-    table.uuid('id').primary()
-    table.foreign('user_id').references('id').inTable('users')
-    table.string('title').notNullable()
-    table.string('description').notNullable()
-    table.enum('is_diet', ['true', 'false']).defaultTo('false')
-    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
-    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
-  })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('meals').dropTable('users')
+  return knex.schema.dropTable('users')
 }
---
+++ db/migrations/20231130094951_create-meals.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary()
+    table.uuid('user_id').references('id').inTable('users')
+    table.string('title').notNullable()
+    table.string('description').notNullable()
+    table.enum('is_diet', ['true', 'false']).defaultTo('false')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
+}
